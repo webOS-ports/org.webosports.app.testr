0a5566387a67f5ad604ee291cc5a0a08afcee51d
Simon Busch <morphis@gravedo.de>
2013-05-07 15:09:55 +0200

    Initial version of the new testing app based on the settings app
    Signed-off-by: Simon Busch <morphis@gravedo.de>

---
+++ source/App.js
@@ -0,0 +1,132 @@
+enyo.kind({
+	name: "ListItem",
+	classes: "list-item",
+	layoutKind: "FittableColumnsLayout",
+	handlers: {
+		onmousedown: "pressed",
+		ondragstart: "released",
+		onmouseup: "released",
+	},
+	published: {
+		icon: "",
+		title: ""
+	},
+	components:[
+		{name: "ItemIcon", kind: "Image", style: "height: 100%"},
+		{name: "ItemTitle", style: "padding-left: 10px;"},
+	],
+	create: function() {
+		this.inherited(arguments);
+		this.$.ItemIcon.setSrc(this.icon);
+		this.$.ItemTitle.setContent(this.title);
+	},
+	pressed: function() {
+		this.addClass("onyx-selected");
+	},
+	released: function() {
+		this.removeClass("onyx-selected");
+	}
+});
+
+enyo.kind({
+	name: "EmptyPanel",
+	layoutKind: "FittableRowsLayout",
+	style: "background-color: #555;",
+	components:[
+		{kind: "onyx.Toolbar"},
+		{fit: true}
+	]
+});
+
+enyo.kind({
+	name: "AppPanels",
+	kind: "Panels",
+	realtimeFit: true,
+	arrangerKind: "CollapsingArranger",
+	classes: "app-panels",
+	components:[
+		{name: "MenuPanel",
+		style: "width: 33%",
+		layoutKind: "FittableRowsLayout",
+		components:[
+			{kind: "PortsHeader",
+			title: "Testr",
+			taglines: [
+				"Testing time!!"
+			]},
+			{kind: "Scroller",
+			horizontal: "hidden",
+			classes: "enyo-fill",
+			style: "background-image:url('assets/bg.png')",
+			fit: true,
+			touch: true,
+			components:[
+				//Connectivity
+				{kind: "onyx.Toolbar", classes: "list-header", content: "Test cases"},
+				{kind: "ListItem", icon: "icon.png", title: "Empty", ontap: "openEmptyPanel"},
+			]},
+		]},
+		{name: "ContentPanels",
+		kind: "Panels",
+		arrangerKind: "CardArranger",
+		draggable: false,
+		index: 1,
+		components:[
+			{kind: "EmptyPanel"},
+		]},
+	],
+	openEmptyPanel: function(inSender) {
+		this.$.ContentPanels.setIndex(1);
+
+		if (enyo.Panels.isScreenNarrow())
+			this.setIndex(1);
+	}
+});
+
+enyo.kind({
+	name: "App",
+	layoutKind: "FittableRowsLayout",
+	components: [
+		{kind: "Signals",
+		ondeviceready: "deviceready",
+		onbackbutton: "handleBackGesture",
+		onCoreNaviDragStart: "handleCoreNaviDragStart",
+		onCoreNaviDrag: "handleCoreNaviDrag",
+		onCoreNaviDragFinish: "handleCoreNaviDragFinish",},
+		{name: "AppPanels", kind: "AppPanels", fit: true},
+		{kind: "CoreNavi", fingerTracking: true}
+	],
+	//Handlers
+	reflow: function(inSender) {
+		this.inherited(arguments);
+		if(enyo.Panels.isScreenNarrow()) {
+			this.$.AppPanels.setArrangerKind("CoreNaviArranger");
+			this.$.AppPanels.setDraggable(false);
+			this.$.AppPanels.$ContentPanels.addStyles("box-shadow: 0");
+		}
+		else {
+			this.$.AppPanels.setArrangerKind("CollapsingArranger");
+			this.$.AppPanels.setDraggable(true);
+			this.$.AppPanels.$.ContentPanels.addStyles("box-shadow: -4px 0px 4px rgba(0,0,0,0.3)");
+		}
+	},
+	handleBackGesture: function(inSender, inEvent) {
+		this.$.AppPanels.setIndex(0);
+	},
+	handleCoreNaviDragStart: function(inSender, inEvent) {
+		this.$.AppPanels.dragstartTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	handleCoreNaviDrag: function(inSender, inEvent) {
+		this.$.AppPanels.dragTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	handleCoreNaviDragFinish: function(inSender, inEvent) {
+		this.$.AppPanels.dragfinishTransition(this.$.AppPanels.draggable == false ? this.reverseDrag(inEvent) : inEvent);
+	},
+	//Utility Functions
+	reverseDrag: function(inEvent) {
+		inEvent.dx = -inEvent.dx;
+		inEvent.ddx = -inEvent.ddx;
+		inEvent.xDirection = -inEvent.xDirection;
+		return inEvent;
+	}
+});
