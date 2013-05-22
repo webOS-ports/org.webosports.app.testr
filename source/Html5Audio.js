fd156a1dbd81dff8930760139afdfb5b8c9b6989
Simon Busch <morphis@gravedo.de>
2013-05-22 11:36:56 +0200

    Add groupbox for html5 audio test case
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- source/Html5Audio.js
+++ source/Html5Audio.js
@@ -51,9 +51,14 @@ enyo.kind({
 			fit: true,
 			components: [
 				{tag: "div", style: "padding: 35px 10% 35px 10%;", components: [
-					{content: "Audio"},
-					{name: "AudioPlayer", kind: "enyo.Audio", preload: false, src: "file:///usr/palm/sounds/alert.wav"},
-					{kind: "onyx.Button", style: "width: 100%;", content: "Play", ontap: "playAudio"}
+					{kind: "onyx.Groupbox", components: [
+					    {kind: "onyx.GroupboxHeader", content: "Sounds"},
+						{components: [
+							{content: "Clicking the button below will play a alarm audio sound to verify that HTML 5 Audio support is working correctly. If you don't hear anything the test has failed.", style: "padding: 5px; color: white"},
+							{name: "AudioPlayer", kind: "enyo.Audio", preload: false, src: "file:///usr/palm/sounds/alert.wav"},
+							{kind: "onyx.Button", style: "width: 100%", content: "Play", ontap: "playAudio"}
+						]},
+					]},
 				]}
 			]
 		},
